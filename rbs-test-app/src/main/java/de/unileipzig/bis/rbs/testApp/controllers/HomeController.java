d113908f2467df17be6590334a3b6a8fd41ad699
Lukas Werner <kontakt@lwerner.de>
2016-05-12 11:23:37 +0200

    Changed home texts

--- rbs-test-app/src/main/java/de/unileipzig/bis/rbs/testApp/controllers/HomeController.java
+++ rbs-test-app/src/main/java/de/unileipzig/bis/rbs/testApp/controllers/HomeController.java
@@ -1,6 +1,12 @@
 package de.unileipzig.bis.rbs.testApp.controllers;
 
+import de.unileipzig.bis.rbs.testApp.model.User;
+import de.unileipzig.bis.rbs.testApp.service.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -11,13 +17,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 
+    @Autowired
+    private UserRepository userRepository;
+
     /**
      * Main entry point (home)
      *
      * @return the view name
      */
     @RequestMapping("/")
-    public String home() {
+    public String home(Model model) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String username = auth.getName();
+        User user = userRepository.findByUsername(username);
+        String name = "Guest";
+        if (user != null) {
+            name = user.getName();
+        }
+        model.addAttribute("name", name);
         return "home";
     }
 
