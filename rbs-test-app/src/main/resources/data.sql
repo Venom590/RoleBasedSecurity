a10aea8ab12d6355f8eb512fae0fb3c5bbd3e7be
Stephan Kemper <stephan.ke@gmx.de>
2016-05-14 14:16:20 +0200

    Added parent id to roles

--- rbs-test-app/src/main/resources/data.sql
+++ rbs-test-app/src/main/resources/data.sql
@@ -2,8 +2,8 @@
 INSERT INTO rbs_users (id, username, password, name) VALUES (1, 'admin', 'admin', 'Stephan Kemper');
 INSERT INTO rbs_users (id, username, password, name) VALUES (2, 'testuser', 'test', 'Lukas Werner');
 
-INSERT INTO rbs_roles (id, name) VALUES (1, 'admin');
-INSERT INTO rbs_roles (id, name) VALUES (2, 'user');
+INSERT INTO rbs_roles (id, parent_id, name) VALUES (1, null, 'admin');
+INSERT INTO rbs_roles (id, parent_id, name) VALUES (2, 1, 'user');
 
 INSERT INTO rbs_users_roles (user_id, role_id) VALUES (1, 1);
 INSERT INTO rbs_users_roles (user_id, role_id) VALUES (2, 2);
